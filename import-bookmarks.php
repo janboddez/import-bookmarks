93e5f046ac4ee3023fafe6ef00127d6e41c4545c
Jan Boddez <jan.boddez@gmail.com>
2019-03-03 13:12:07 +0100

    Minor version bump: added i18n, (some) Gutenberg compatibility

--- import-bookmarks.php
+++ import-bookmarks.php
@@ -4,7 +4,7 @@
  * Description: Import browser bookmarks as WordPress posts.
  * Author: Jan Boddez
  * Author URI: https://janboddez.tech/
- * Version: 0.1
+ * Version: 0.2.1
  */
 
 namespace Import_Bookmarks;
@@ -16,13 +16,52 @@ require_once dirname( __FILE__ ) . '/vendor/netscape-bookmark-parser/NetscapeBoo
  */
 class Bookmarks_Importer {
 	/**
+	 * Holds WordPress default post types, minus 'post' itself.
+	 *
+	 * @since 0.2.1
+	 */
+	private $default_post_types = array(
+		'page',
+		'attachment',
+		'revision',
+		'nav_menu_item',
+		'custom_css',
+		'customize_changeset',
+		'user_request',
+		'oembed_cache',
+		'wp_block',
+	);
+
+	/**
+	 * Allowable post statuses.
+	 *
+	 * @since 0.2.1
+	 */
+	private $post_statuses = array(
+		'publish',
+		'draft',
+		'pending',
+		'private',
+	);
+
+	/**
 	 * Registers actions.
 	 *
 	 * @since 0.1.0
 	 */
 	public function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
 		add_action( 'admin_menu', array( $this, 'create_menu' ) );
 		add_action( 'admin_post_import_bookmarks', array( $this, 'import' ) );
+	}
+
+	/**
+	 * Enables i18n of this plugin.
+	 *
+	 * @since 0.2.0
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'import-bookmarks', false, basename( dirname( __FILE__ ) ) . '/languages' );
 	}
 
 	/**
@@ -62,38 +101,18 @@ class Bookmarks_Importer {
 						<th scope="row"><label for="post-type"><?php _e( 'Post Type', 'import-bookmarks' ); ?></label></th>
 						<td><select name="post_type" id="post-type">
 							<?php
-							$default_post_types = array(
-								//'post',
-								'page',
-								'attachment',
-								'revision',
-								'nav_menu_item',
-								'custom_css',
-								'customize_changeset',
-								'user_request',
-								'oembed_cache',
-								'wp_block',
-							);
-							$post_types = array_diff( get_post_types(), $default_post_types );
+							$post_types = array_diff( get_post_types(), $this->default_post_types );
 							?>
 							<?php foreach ( $post_types as $post_type ) : ?>
-							<option value="<?php $post_type = get_post_type_object( $post_type ); echo esc_attr( $post_type->name ); ?>"><?php echo esc_html( $post_type->labels->singular_name ); ?></option>
+								<option value="<?php $post_type = get_post_type_object( $post_type ); echo esc_attr( $post_type->name ); ?>"><?php echo esc_html( $post_type->labels->singular_name ); ?></option>
 							<?php endforeach; ?>
 						</select></td>
 					</tr>
 					<tr valign="top">
 						<th scope="row"><label for="post-status"><?php _e( 'Post Status', 'import-bookmarks' ); ?></label></th>
 						<td><select name="post_status" id="post-status">
-							<?php
-							$post_statuses = array(
-								'publish',
-								'draft',
-								'pending',
-								'private',
-							);
-							?>
-							<?php foreach ( $post_statuses as $post_status ) : ?>
-							<option value="<?php echo esc_attr( $post_status ); ?>"><?php echo esc_html( ucfirst( $post_status ) ); ?></option>
+							<?php foreach ( $this->post_statuses as $post_status ) : ?>
+								<option value="<?php echo esc_attr( $post_status ); ?>"><?php echo esc_html( ucfirst( $post_status ) ); ?></option>
 							<?php endforeach; ?>
 						</select></td>
 					</tr>
@@ -145,20 +164,10 @@ class Bookmarks_Importer {
 			return;
 		}
 
+		// Default value.
 		$post_type = 'post';
-		$default_post_types = array(
-			//'post',
-			'page',
-			'attachment',
-			'revision',
-			'nav_menu_item',
-			'custom_css',
-			'customize_changeset',
-			'user_request',
-			'oembed_cache',
-			'wp_block',
-		);
-		$post_types = array_diff( get_post_types(), $default_post_types );
+
+		$post_types = array_diff( get_post_types(), $this->default_post_types );
 
 		if ( ! empty( $_POST['post_type'] ) && in_array( $_POST['post_type'], $post_types ) ) {
 			$post_type = $_POST['post_type'];
@@ -166,7 +175,7 @@ class Bookmarks_Importer {
 
 		$post_status = 'publish';
 
-		if ( ! empty( $_POST['post_status'] ) && in_array( $_POST['post_status'], array( 'publish', 'draft', 'pending', 'private' ) ) ) {
+		if ( ! empty( $_POST['post_status'] ) && in_array( $_POST['post_status'], $this->post_statuses ) ) {
 			$post_status = $_POST['post_status'];
 		}
 
@@ -174,16 +183,31 @@ class Bookmarks_Importer {
 		$bookmarks = $parser->parseFile( $uploaded_file['file'] );
 
 		foreach ( $bookmarks as $bookmark ) {
+			$post_title    = sanitize_text_field( $bookmark['title'] );
+			$post_content  = sanitize_text_field( $bookmark['note'] );
+			$post_content .= "\n\n<a href='" . esc_url( $bookmark['uri'] ) . "'>" . $post_title . '</a>';
+			$post_content  = trim( $post_content );
+
+			if ( function_exists( 'use_block_editor_for_post_type' ) && use_block_editor_for_post_type( $post_type ) ) {
+				// Convert to paragraph blocks.
+				$post_content = str_replace(
+					"\n\n",
+					"</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:paragraph -->\n<p>",
+					$post_content
+				);
+				$post_content = "<!-- wp:paragraph -->\n<p>" . $post_content . "</p>\n<!-- /wp:paragraph -->";
+			}
+
 			$post_id = wp_insert_post( array(
-				'post_title'   => sanitize_text_field( $bookmark['title'] ),
-				'post_content' => trim( sanitize_text_field( $bookmark['note'] ) . "\n\n" . esc_url_raw( $bookmark['uri'] ) ),
+				'post_title'   => $post_title,
+				'post_content' => $post_content,
 				'post_status'  => $post_status,
 				'post_type'    => $post_type,
 				'post_date'    => get_date_from_gmt( date( 'Y-m-d H:i:s', $bookmark['time'] ), 'Y-m-d H:i:s' ),
 			) );
 
 			if ( $post_id && post_type_supports( $post_type, 'custom-fields' ) ) {
-				// Store URL itself in a custom field, too, for possible future use.
+				// Also store actual URL, in a custom field.
 				update_post_meta( $post_id, 'import_bookmarks_uri', esc_url_raw( $bookmark['uri'] ) );
 			}
 		}
